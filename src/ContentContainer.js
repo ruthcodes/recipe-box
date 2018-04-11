c0bb930810c89fb1a195091746b23911539a7817
Ruth Ashleigh <notruth@notruth.com>
2018-04-10 21:55:13 -0700

    Changed li creating map to arrow function

--- src/ContentContainer.js
+++ src/ContentContainer.js
@@ -10,10 +10,16 @@ export function ContentContainer(props){
           <Col className="recipeCol" xs={12} sm={12} md={12} lg={12} xl={12}>
             <ul>
             {
-              Object.keys(props.name).map(function(key) {
-                return <li key={key} className={props.active === key ? 'activeRecipe': null} 
-                onClick={props.expandRecipe.bind(this, key)} >{props.name[key]}<br/><br/>{props.ingredients[key]}<br/><br/> <Button onClick={props.handleEditing.bind(this,key)}>Edit</Button><Button className="btn-danger">Delete</Button></li>
-              })
+              Object.keys(props.name).map((key) => (
+                <li key={key} className={props.active === key ? 'activeRecipe': null} onClick={props.expandRecipe.bind(this, key)} >
+                  {props.name[key]}
+                  <br/><br/>
+                  {props.ingredients[key]}
+                  <br/><br/>
+                  <Button onClick={props.handleEditing.bind(this,key)}>Edit</Button>
+                  <Button onClick={() => props.deleteRecipe(key)}className="btn-danger">Delete</Button>
+                </li>
+              ))
             }
             </ul>
           </Col>
